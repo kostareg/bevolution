d6f71269001f67a22fc2a277af7e96a5543dba11
kostareg <areg.kostanyan@gmail.com>
2025-08-26 19:19:30 -0400

    feat: initial physics

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,59 @@
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 
 #[derive(Component)]
 struct Blob;
 
 impl Blob {
-    fn step(&self, pos: Vec3) -> Vec3 {
-        let delta = (rand::random::<f32>() - 0.5) / 100.;
+    fn step(&self, force: Vec3) -> Vec3 {
+        let delta = rand::random::<f32>() - 0.5;
         Vec3 {
-            x: (pos.x + delta).clamp(-2., 2.),
-            y: (pos.y + delta).clamp(-2., 2.),
-            z: (pos.y + delta).clamp(-2., 2.),
+            x: (force.x + delta).clamp(-20., 20.),
+            y: (force.y + delta).clamp(-20., 20.),
+            z: (force.y + delta).clamp(-20., 20.),
         }
     }
 }
 
-fn spawn_blobs(
+fn spawn_environment(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    for i in 0..100 {
-        commands.spawn((
-            Blob,
-            Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-            MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-            Transform::from_xyz(i as f32 * 1.1, 0., 0.),
-        ));
-    }
+    let hx = 3.;
+    let hy = 3.;
+    let hz = 3.;
+
+    let verts = vec![
+        Vec3::new(-hx, -hy, -hz),
+        Vec3::new( hx, -hy, -hz),
+        Vec3::new( hx,  hy, -hz),
+        Vec3::new(-hx,  hy, -hz),
+        Vec3::new(-hx, -hy,  hz),
+        Vec3::new( hx, -hy,  hz),
+        Vec3::new( hx,  hy,  hz),
+        Vec3::new(-hx,  hy,  hz),
+    ];
+
+    let indices = vec![
+        [0,1,2], [0,2,3],
+        [4,6,5], [4,7,6],
+        [0,4,5], [0,5,1],
+        [3,2,6], [3,6,7],
+        [1,5,6], [1,6,2],
+        [0,3,7], [0,7,4],
+    ];
+
+    commands.spawn((
+        RigidBody::Fixed,
+        Collider::trimesh(verts, indices).unwrap(),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(3.)))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(0, 144, 255))),
+        Transform::from_xyz(0., -3., 0.),
+    ));
 
     commands.spawn((
         PointLight {
@@ -42,21 +69,39 @@ fn spawn_blobs(
     ));
 }
 
-fn step(
-    mut query: Query<(&Blob, &mut Transform)>,
+fn spawn_blobs(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    for (blob, mut transform) in &mut query {
-        transform.translation = blob.step(transform.translation);
+    for _ in 0..100 {
+        commands.spawn((
+            Blob,
+            Collider::cuboid(0.1, 0.1, 0.1),
+            RigidBody::Dynamic,
+            ExternalForce { force: Vec3::ZERO, torque: Vec3::ZERO },
+            Restitution::coefficient(0.7),
+            Mesh3d(meshes.add(Cuboid::new(0.1, 0.1, 0.1))),
+            MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+            Transform::from_xyz(0., 0., 0.),
+        ));
     }
 }
 
-fn render() {}
+fn step(
+    mut query: Query<(&Blob, &mut ExternalForce)>
+) {
+    for (blob, mut ext_force) in query.iter_mut() {
+        ext_force.force = blob.step(ext_force.force);
+    }
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, spawn_blobs)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, (spawn_environment, spawn_blobs))
         .add_systems(FixedUpdate, step)
-        .add_systems(RunFixedMainLoop, (render.in_set(RunFixedMainLoopSystem::AfterFixedMainLoop)))
         .run();
 }
